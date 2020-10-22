8f9f96626aeb86184268ff30ae769784972923df
Richard Leung <michelleli@MMac.local>
2020-10-22 10:00:38 -0400

    [hello.js] Add functions to file

---
+++ hello.js
@@ -0,0 +1,32 @@
+function sayHello(name) {
+    let msg = 'Hello,' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('world'));
+
+function sayHello(name) {
+    let msg = 'Hello,' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Gary'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Mary'));
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('Anna'))
+
+function sayHello(name) {
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
